67114c3d416f4dbb633addb4a20a5f37c2eed717
DexiizStore <92228307+DexiizStore@users.noreply.github.com>
2022-06-07 02:33:04 -0300

    update react quiz

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/App.css'
+import Quiz from './componentes/Quiz.jsx'
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <h1>Quiz do Boizão</h1>
+      <Quiz />
+    </div>
+  )
+}
+
+
+export default App
